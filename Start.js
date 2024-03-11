199ae4a76a8504a90baafd4d2d1d767371b4dae6
Daphne <Daphnepaterek@gmail.com>
2024-03-11 12:50:07 +0100

    inlog page

--- Start.js
+++ Start.js
@@ -1,30 +1,4 @@
 
-class CustomHeader extends HTMLElement {
-    shadowRoot;
-
-    constructor() {
-        super();
-        this.shadowRoot = this.attachShadow({mode: 'open'});
-        const p = document.createElement('p');
-        p.innerHTML += 'toasty';
-        this.shadowRoot.appendChild(p);
-
-    }
-}
-
-customElements.define('custom-header', CustomHeader);
-document.getElementById("toasty").addEventListener("click", showToasty);
-
-async function removeCustomTitle(custom) {
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    custom.remove(); 
-}
-
-function showToasty(){ 
-    const customHeader = new CustomHeader();
-    document.body.appendChild(customHeader);
-    removeCustomTitle(customHeader);
-}
 
 
 
